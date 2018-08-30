9ec22c503ae771c39d08578017be95a3c0e0432e
Claudio Scordino <claudio@evidence.eu.com>
2018-08-30 16:41:49 +0200

    Found Ethernet device

--- inmates/demos/x86/imech-demo.c
+++ inmates/demos/x86/imech-demo.c
@@ -11,14 +11,56 @@
  */
 
 #include <inmate.h>
-#define UART_BASE	0x3F8
+
+#include "imech-demo.h"
+
+static int eth_pci_probe (struct eth_device *dev)
+{
+        u64 bar;
+        int bdf;
+
+        bdf = pci_find_device(ETH_VENDORID, ETH_DEVICEID, 0);
+        if (bdf < 0) {
+                print("ERROR: no device found\n");
+                return -1;
+        }
+        print("found %04x:%04x at %02x:%02x.%x\n",
+                        pci_read_config(bdf, PCI_CFG_VENDOR_ID, 2),
+                        pci_read_config(bdf, PCI_CFG_DEVICE_ID, 2),
+                        bdf >> 8, (bdf >> 3) & 0x1f, bdf & 0x3);
+
+        /* Read Base Address Register in the configuration */
+        bar = pci_read_config(bdf, PCI_CFG_BAR, 4);
+        if ((bar & 0x6) == 0x4)
+                bar |= (u64)pci_read_config(bdf, PCI_CFG_BAR + 4, 4) << 32;
+
+        /* Map BAR in the virtual memory */
+        dev->bar_addr = (void *)(bar & ~0xfUL);
+        map_range(dev->bar_addr, PAGE_SIZE, MAP_UNCACHED);
+        print("BAR at %p\n", dev->bar_addr);
+
+        /* Set MSI IRQ vector */
+        pci_msi_set_vector(bdf, ETH_IRQ_VECTOR);
+
+        pci_write_config(bdf, PCI_CFG_COMMAND,
+                        PCI_CMD_MEM | PCI_CMD_MASTER, 2);
+
+        print("PCI device succesfully initialized\n");
+        return 0;
+}
+
+
 
 void inmate_main(void)
 {
+	struct eth_device dev;
+
 	printk("Hello world!\n");
 	printk("Hello world!\n");
 	printk("Hello world!\n");
 	printk("Hello world!\n");
+
+	eth_pci_probe(&dev);
 
 	asm volatile("hlt");
 }
