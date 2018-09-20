4ebd848984dd08c3748a105ea33ee2802d95ce5f
Claudio Scordino <claudio@evidence.eu.com>
2018-09-20 13:20:25 +0200

    Fix STATUS.SPEED

--- inmates/demos/x86/i210.h
+++ inmates/demos/x86/i210.h
@@ -29,7 +29,7 @@
 	#define E1000_STATUS_LU		(1 << 1)	// Link up
 	#define E1000_STATUS_SPEED	(3 << 6)	// Link speed
 	#define E1000_STATUS_SPEED_10	(0 << 6)	// Speed 10  Mb/s
-	#define E1000_STATUS_SPEED_100	(4 << 6)	// Speed 100 Mb/s
+	#define E1000_STATUS_SPEED_100	(1 << 6)	// Speed 100 Mb/s
 #define E1000_CTRL_EXT	0x00018	// Extended Device Control - RW
 	#define E1000_CTRL_EXT_BYPS	(1 << 15)	// Speed Select Bypass
 	#define E1000_CTRL_EXT_SD_LP	(1 << 18)	// SerDes Low Power Enable
